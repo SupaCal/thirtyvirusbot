978c1a04a32c38d4ef22aef5d8921a8273006f27
calvi <calvinlaurenson15@gmail.com>
2020-11-26 11:53:43 -0800

    sadge streamlabs

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ var { google } = require("googleapis");
 const { Client } = require("discord.js");
 require("dotenv").config();
 
-
 const youtube = google.youtube({ auth: process.env.GOOGLE_KEY, version: "v3" });
 
-var config = JSON.parse(fs.readFileSync("discconfig.json"));
+var config = JSON.parse(fs.readFileSync("config.json"));
 var isReady = false;
 var client = new Client();
 client.on("ready", () => {
   isReady = true;
 });
 client.on("message", (msg) => {
+  msg.channel.send();
   if (msg.channel.id == config.channelids.mee6) {
-    if (msg.content.includes("youtu")) {
-      var vdid = msg.content.match(
-        /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
-      );
-      //console.log(vdid);
+    var vdid = msg.content.match(
+      /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+    );
+    //console.log(vdid);
+    if (vdid) {
       var link = "https://" + vdid[0];
       youtube.videos.list({ id: vdid[1], part: "snippet" }, (err, response) => {
         //console.log(err);
@@ -46,8 +46,29 @@ client.on("message", (msg) => {
   }
 });
 
+if (config.streamlabs.enabled) {
+  if (process.env.WS_KEY) {
+    var streamlabs = require("socket.io-client")(
+      `https://sockets.streamlabs.com?token=${process.env.WS_KEY}`
+    );
+    streamlabs.on("event", (slevent) => {
+      console.log(slevent);
+      if (slevent.type === "donation") {
+        console.log(slevent.message[0].name.toString());
+        console.log(`Name = ${slevent.message[0].name.toString()}`);
+        client.channels.cache
+          .get("781325325417709629")
+          .send(slevent.message[0].name.toString());
+        //sendMessage('781325325417709629', slevent.message[0].from)
+      }
+      //sendMessage('781325325417709629', slevent)
+    });
+  } else console.log("No Streamlabs websocket key provided");
+}
+
 function sendMessage(chid, msg) {
   if (isReady) {
+    console.log("sendmessage called");
     client.channels.cache.get(chid).send(msg);
   }
 }
@@ -72,6 +93,5 @@ async function isLive(vdid) {
     return true;
   }
 }
-
 
 client.login(process.env.BOT_TOKEN);
